dc11842c38177eb744c3e7e5e506c1b61fd12777
Charles Elvira <ninou14fr@ufl.edu>
2017-08-27 01:57:36 -0400

    Implementation of forms and filter through search list

--- listingController.js
+++ listingController.js
@@ -7,7 +7,22 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
+    $scope.addListing = function() 
+	{
+		var code, name, lat, lon, add, entry;
+		
+		code = $scope.newListing.code;
+		name = $scope.newListing.name;
+		lat = $scope.newListing.latitude;
+		lon = $scope.newListing.longitude;
+		add = $scope.newListing.address;
+		
+		entry = '{ "code": "' + code + '", "name": "' + name + '"';
+		if (lat && lon) entry += ',"coordinates": {"latitude": "'+ lat + '", "longitude": "'+ lon +'" }';
+		if (add) entry += ',"address": "' + add + '"';
+		entry += ' }';
+		$scope.listings.push(JSON.parse(entry));
+	};
     $scope.deleteListing = function(index) {};
     $scope.showDetails = function(index) {};
   }
